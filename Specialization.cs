28b97891d902616e0a689331429942e590880ee2
marta-krzyk-dev <marta-krzyk-dev@users.noreply.github.com>
2017-06-11 10:34:19 +0200

    Add files via upload

---
+++ Specialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace MedicalClinic.Models
+{
+    public class Specialization
+    {
+        public int SpecializationId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        //[Required]
+        [StringLength(550)]
+        public string Description { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<Doctor> Doctors { get; set; }
+    }
+}
